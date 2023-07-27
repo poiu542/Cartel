2ad9d08cb5693633a40f636033d6f17fcedbf5cc
석민혁 <tjralsgur12@naver.com>
2023-07-27 14:26:30 +0900

    feat : main 페이지 구현

--- frontend/src/routes/Main.tsx
+++ frontend/src/routes/Main.tsx
@@ -1,16 +1,170 @@
 import React, { useState } from 'react'
 import Carousel from '../components/Carousel'
 import NavbarLogin from '../components/NavbarLogin'
-// import NavbarLogout from '../components/NavbarLogout'
+import CounselorCard from '../components/CounselorCard'
+import PreviewBox from '../components/PreviewBox'
+import Footer from '../components/Footer'
 
 export const Main = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const onCardClick = () => {
+    alert('상담 정보 보기')
+  }
+  const ViewAll = () => {
+    alert('더보기')
+  }
 
   return (
     <div>
-      {/* {isLoggedIn ? <NavbarLogin /> : <NavbarLogout />} */}
-      <NavbarLogin />
-      <div>Main</div>
+      <div className="Navbar">
+        <NavbarLogin />
+      </div>
+      <div className="Carousel">
+        <Carousel />
+      </div>
+      <div
+        className="blank"
+        style={{
+          height: '200px',
+          width: '100%',
+        }}
+      ></div>
+      <div
+        className="counsel list"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+          backgroundColor: '#ECF9FF',
+          height: '409px',
+          width: '100%',
+          gap: '100px',
+        }}
+      >
+        <CounselorCard
+          onCardClick={onCardClick}
+          name="석민혁"
+          grade={4.8}
+          gradeCount={51}
+          introduce="족구왕이 될 사나이"
+        />
+        <CounselorCard
+          onCardClick={onCardClick}
+          name="이순신"
+          grade={4.7}
+          gradeCount={45}
+          introduce="해상 전략가"
+        />
+        <CounselorCard
+          onCardClick={onCardClick}
+          name="박찬호"
+          grade={4.9}
+          gradeCount={53}
+          introduce="야구 선수"
+        />
+      </div>
+      <div
+        className="blank"
+        style={{
+          height: '200px',
+          width: '100%',
+        }}
+      ></div>
+      <div
+        className="article list"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+          backgroundColor: '#ECF9FF',
+          height: '600px',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '50%',
+          }}
+        >
+          <div
+            className="BEST article box"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '13px',
+              backgroundColor: 'white',
+              width: '313px',
+              height: '70px',
+              color: '#40BFFF',
+              margin: '30px',
+              fontSize: '20px',
+            }}
+          >
+            BEST 게시글
+          </div>
+          <PreviewBox
+            title="BEST 게시글"
+            posts={[
+              { title: '[공지] 상담일정 변경 안내' },
+              { title: '공지2' },
+              { title: '공지3' },
+              { title: '공지4' },
+            ]}
+            onClick={ViewAll}
+          />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '50%',
+          }}
+        >
+          <div
+            className="HOT article box"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '13px',
+              backgroundColor: 'white',
+              width: '313px',
+              height: '70px',
+              color: '#40BFFF',
+              margin: '30px',
+              fontSize: '20px',
+            }}
+          >
+            HOT 게시글
+          </div>
+          <PreviewBox
+            title="HOT 게시글"
+            posts={[
+              { title: '[공지] 상담일정 변경 안내' },
+              { title: '공지2' },
+              { title: '공지3' },
+              { title: '공지4' },
+            ]}
+            onClick={ViewAll}
+          />
+        </div>
+      </div>
+      <div
+        className="blank"
+        style={{
+          height: '200px',
+          width: '100%',
+        }}
+      ></div>
+      <Footer />
     </div>
   )
 }
