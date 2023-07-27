9d26eccf51590ad9d69a4191af278ddf4b8d6ce7
석민혁 <tjralsgur12@naver.com>
2023-07-27 13:34:01 +0900

    git merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App(): React.ReactElement {
   const [inputValue, setInputValue] = useState('')
--- frontend/src/routes/Main.tsx
+++ frontend/src/routes/Main.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import Carousel from '../components/Carousel'
 import NavbarLogin from '../components/NavbarLogin'
-import NavbarLogout from '../components/NavbarLogout'
+// import NavbarLogout from '../components/NavbarLogout'
 
 export const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
     <div>
-      {isLoggedIn ? <NavbarLogin /> : <NavbarLogout />}
+      {/* {isLoggedIn ? <NavbarLogin /> : <NavbarLogout />} */}
+      <NavbarLogin />
       <div>Main</div>
     </div>
   )
