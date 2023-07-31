7d33af695a58c7a7bad3d28bc542d2d6f4fb177c
xogmamoc <xogmamoc@naver.com>
2023-07-31 16:09:04 +0900

    feat : 회원가입 페이지 수정
    일반 회원가입, 상담사 회원가입 버튼 수정 완료

--- frontend/src/routes/CounselorDetail.tsx
+++ frontend/src/routes/CounselorDetail.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-export const CounselorDetail = () => {
-  return <div>Counselor</div>
+export const CounselorDetail: React.FC = () => {
+  let { id } = useParams<{ id: string }>()
+
+  // 가져오는 id로 상담사 정보를 가져와야 함
+
+  return (
+    <>
+      <div>상담사 아이디: {id}</div>
+    </>
+  )
 }
