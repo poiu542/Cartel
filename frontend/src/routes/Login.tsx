84dab4fac9a4cf580f251aaca45232dc3e2f0a5a
이경원 <jbs6606@naver.com>
2023-07-31 15:45:04 +0900

    1

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import NavbarLogout from '../components/NavbarLogout'
 
 export const Login = () => {
-  return <div>login</div>
+  return (
+    <div className="login">
+      <NavbarLogout />
+      <div className="login-group">
+        <div>
+          <div className="normal-login"></div>
+          <div className="counseller-login"></div>
+          <h1 className="text-1">일반 로그인</h1>
+          <div className="text-2">상담사 로그인</div>
+        </div>
+      </div>
+    </div>
+  )
 }
