12cff290704f2f94b3282a3a2d2046e932614926
안도희 <adh1120@naver.com>
2023-08-01 17:10:51 +0900

    feat: article

---
+++ BACKEND/src/main/java/com/ssafy/cartel/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.cartel.repository;
+
+import com.ssafy.cartel.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+
+}
