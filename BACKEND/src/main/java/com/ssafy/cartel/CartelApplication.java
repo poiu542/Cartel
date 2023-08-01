5cab4028caa67156a01302a94357f732d2cc8b37
안도희 <adh1120@naver.com>
2023-08-01 13:24:49 +0900

    test: backend jenkins

--- BACKEND/src/main/java/com/ssafy/cartel/CartelApplication.java
+++ BACKEND/src/main/java/com/ssafy/cartel/CartelApplication.java
@@ -2,11 +2,9 @@ package com.ssafy.cartel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 
-//@SpringBootApplication
+@SpringBootApplication
 public class CartelApplication {
 
 	public static void main(String[] args) {
