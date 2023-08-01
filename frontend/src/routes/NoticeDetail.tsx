19323d19529f8ba6a906538bd09fdd22e79f897f
kimdongwoo <siwol406@gmail.com>
2023-08-01 15:09:16 +0900

    feat: NoticeWrite, Notice Detail 수정

--- frontend/src/routes/NoticeDetail.tsx
+++ frontend/src/routes/NoticeDetail.tsx
@@ -14,7 +14,9 @@ export const NoticeDetail = () => {
           alignItems: 'center',
         }}
       >
+        {/* title 작성 */}
         <h1 style={{ marginLeft: '50px' }}>오늘 금약 25일째....</h1>
+        {/* 작성, 작성자 정보  */}
         <div style={{ marginBottom: '10px', marginLeft: '50px' }}>
           작성자<span style={{ marginLeft: '30px' }}>작성날짜</span>
           <span style={{ marginLeft: '30px' }}>레벨</span>
@@ -29,6 +31,7 @@ export const NoticeDetail = () => {
           src={process.env.PUBLIC_URL + '/image/seulyoon.jpg'}
           alt="설윤"
         />
+        {/* context작성 */}
         <p>
           펜타닐, 메스암페타민, 마리화나 등등 온갖마약을 하고 살았는데 이번엔
           제대로 끊어보려고 자조모임 하고있습니다. 모두 화이팅!{' '}
