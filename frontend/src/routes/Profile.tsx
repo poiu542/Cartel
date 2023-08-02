5a86f7f236d4712f46459a1fa144a98b35dd478f
이경원 <jbs6606@naver.com>
2023-08-02 17:07:06 +0900

    refactor

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,83 +1,133 @@
 import React from 'react'
-import styled from 'styled-components'
-import NavbarLogin from '../components/NavbarLogin'
+import NavbarLogout from '../components/NavbarLogout'
+import { styled } from 'styled-components'
+import Input from '../components/Input'
 
-// Base64로 인코딩된 이미지 데이터
-const base64Image =
-  'iVBORw0KGgoAAAANSUhEUgAAAZMAAAE4CAYAAACexx5MAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAADghSURBVHhe7Z0LlF1VeYBdfdiXDx5VikKp2ofWtrSKraCtUFtsKylVWgFrhRaFUlRQqKBQoYUWbKnQolUQC1ZFKVTkEVmlyiuFiBkgIyaaCSSBkITmNckkM5PMEHbvt+f+w8n...'
+type Props = {}
 
-// 원형 이미지
-const CircleImage = styled.img`
+const GridLayout = styled.div`
+  max-width: 1280px;
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 300px auto;
+  gap: 20px;
+  padding: 0 24px;
+  margin: 50px auto 0;
+`
+
+const GridProfileArea = styled.div`
+  display: grid;
+  grid-template-rows: 350px auto;
+  align-items: center; /* 가로축 가운데 정렬을 위해 추가 */
+`
+
+const GridInputArea = styled.div`
+  display: flex;
+  width: 480px;
+  flex-direction: column;
+  align-items: center; /* 가로축 가운데 정렬을 위해 수정 */
+  gap: 6px;
+`
+
+const ProfileImage = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   width: 100%;
-  height: 100%;
-  object-fit: cover; /* 이미지 비율 유지하면서 컨테이너 크기에 맞춤 */
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+
+  > img {
+    aspect-ratio: 1 / 1;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  > h2 {
+    width: 100%;
+    text-align: center;
+  }
 `
 
-// 원형 이미지 칸을 감싸는 컨테이너
-const CircleImageContainer = styled.div`
-  width: 100px;
-  height: 100px;
-  border-radius: 50%; /* 반지름을 50%로 지정하여 원형으로 만듦 */
-  overflow: hidden; /* 이미지가 컨테이너를 넘어가지 않도록 설정 */
-  flex-shrink: 0; /* flexbox 내에서 크기 변화 방지 */
-  margin-right: 20px; /* 원형 이미지 오른쪽 여백 설정 */
-`
+const ProfileMenu = styled.ul`
+  width: 100%;
+  border-radius: 20px;
+  border: 2px solid #ccc;
+  padding: 5px;
 
-// Input 스타일
+  > li {
+    border-bottom: 2px solid #ccc;
+    padding: 5px;
+    margin: 0px;
+
+    > h3 {
+      margin: 0px;
+      padding: 0px;
+    }
+
+    &:last-child {
+      border: 0;
+    }
+  }
+`
 const ProfileInput = styled.input`
   border: 1px solid var(--gray-300, #dee2e6);
+  border-radius: 6px;
   background: var(--default-white, #fff);
-  color: var(--gray-500, #adb5bd);
   font-family: Inter;
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: 20px;
-  padding: 8px;
 `
-
-// Input 요소를 담는 컨테이너
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px; /* input 요소 간격 설정 */
+const ProfileMenuWrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  display: grid; /* Grid 컨테이너로 설정 */
+  grid-template-columns: auto auto; /* 두 개의 자식 요소를 자동 크기로 배치 */
+  gap: 6px; /* 컬럼 사이의 간격 설정 */
 `
+const nickname = '이경원'
 
-const CircleImageContainerWithOrder = styled(CircleImageContainer)`
-  order: -1; /* 이미지의 순서를 앞으로 변경 */
-`
-
-export const Profile: React.FC = () => {
-  const imageSrc = `data:image/png;base64,${base64Image}`
-
+export const Profile = (props: Props) => {
   return (
-    <div>
-      <NavbarLogin />
-<<<<<<< HEAD
-      <div>
-        <ProfileInput></ProfileInput>
-      </div>
-      <div>
-        <ProfileInput></ProfileInput>
-      </div>
-      <div>
-        <ProfileInput></ProfileInput>
-      </div>
-      <div>
-        <ProfileInput></ProfileInput>
-      </div>
-=======
-      <InputContainer>
-        <CircleImageContainerWithOrder>
-          <CircleImage src="/image/seulyoon.jpg" alt="Circle Image" />
-        </CircleImageContainerWithOrder>
-        <ProfileInput placeholder="1" />
-        <ProfileInput placeholder="1" />
-        <ProfileInput placeholder="1" />
-        <ProfileInput placeholder="1" />
-      </InputContainer>
->>>>>>> 8c818c278d5bf14721aa651837c5dd38bda09f90
-    </div>
+    <section>
+      <NavbarLogout />
+      <GridLayout>
+        <GridProfileArea>
+          <ProfileImage>
+            <img src="/image/seulyoon.jpg" alt="1" />
+            <h2>{nickname}</h2>
+          </ProfileImage>
+        </GridProfileArea>
+        <GridInputArea>
+          <ProfileInput />
+          <ProfileInput />
+          <ProfileInput />
+          <ProfileInput />
+        </GridInputArea>
+        <ProfileMenuWrapper>
+          <ProfileMenu>
+            <li>
+              <h3>내가 쓴 게시글</h3>
+              <span>{'>>'}</span>
+            </li>
+            <li>
+              <h3>내가 쓴 댓글</h3>
+              <span>{'>>'}</span>
+            </li>
+            <li>
+              <h3>내 상담</h3>
+              <span>{'>>'}</span>
+            </li>
+          </ProfileMenu>
+          <ProfileInput />
+        </ProfileMenuWrapper>
+      </GridLayout>
+    </section>
   )
 }
