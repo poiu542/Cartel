b73499959e40dea4bd9c0812a8946e1936aacf98
석민혁 <tjralsgur12@naver.com>
2023-08-03 09:35:44 +0900

    refector : navbar 로그인 여부에 따라 상태 변경 되도록 변경

---
+++ frontend/src/routes/CounselJournal.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const CounselJournal = () => {
+  return <div>CounselReady</div>
+}
---
+++ frontend/src/routes/CounselJournalCreate.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const CounselJournalCreate = () => {
+  return <div>CounselReady</div>
+}
