c5547840f04154c8a3b68dabf31058faa07a4d7e
안도희 <adh1120@naver.com>
2023-08-07 17:24:49 +0900

    file :  cors

--- BACKEND/src/main/java/com/ssafy/cartel/config/WebSecurityConfig.java
+++ BACKEND/src/main/java/com/ssafy/cartel/config/WebSecurityConfig.java
@@ -1,11 +1,26 @@
 package com.ssafy.cartel.config;
 
+import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/","/login","/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .build();
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
