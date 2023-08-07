eb537f28c35645525f8cae60b5db0e0dd81d9e44
안도희 <adh1120@naver.com>
2023-08-07 17:00:45 +0900

    Update BACKEND/build.gradle, BACKEND/src/test/java/com/ssafy/cartel/CartelApplicationTests.java

--- BACKEND/src/test/java/com/ssafy/cartel/CartelApplicationTests.java
+++ BACKEND/src/test/java/com/ssafy/cartel/CartelApplicationTests.java
@@ -1,9 +1,9 @@
-//package com.ssafy.cartel;
+package com.ssafy.cartel;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class CartelApplicationTests {
 
 	//@Test
