1f4d72710884cc74dccd74ae6dcf291329303522
박태흠 <xomamoc@naver.com>
2023-08-10 03:54:13 +0900

    feat : 커뮤니티 페이지들 디테일 수정

--- frontend/src/routes/QnaDetail.tsx
+++ frontend/src/routes/QnaDetail.tsx
@@ -10,60 +10,15 @@ import { BoardData } from '../model/board'
 import Comment from './../components/Comment'
 import { CenteredDiv, SpacedDiv, StyledForm } from '../components/Write'
 import styled from '@emotion/styled'
-
-export const CustumDiv = styled.div`
-  width: 80%;
-  height: 90%;
-  border-top: 4px solid #3b478f;
-  border-left: 1px solid gray;
-  border-right: 1px solid gray;
-  border-bottom: 1px solid gray;
-`
-
-// Emotion 스타일 컴포넌트
-const ArticleContainer = styled.div`
-  margin: 50px 150px;
-  min-height: 800px;
-  border: 1px solid gray;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-`
-
-const ArticleHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const ArticleTitle = styled.h1`
-  font-size: 1.5rem;
-  font-weight: 700;
-`
-
-const ArticleMeta = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  padding-bottom: 10px;
-  margin-bottom: 20px;
-  border-bottom: 1px solid gray;
-`
-
-const ArticleContent = styled.p`
-  font-size: 1rem;
-  line-height: 1.5;
-  min-height: 300px;
-  padding: 20px;
-  border: 1px solid #e0e0e0;
-`
-
-const ButtonGroup = styled.div`
-  display: flex;
-  justify-content: end;
-  gap: 10px;
-  margin-top: 20px;
-`
+import { formatDateDetail } from '../utils/dateUtils'
+import {
+  ArticleContainer,
+  ArticleHeader,
+  ArticleContent,
+  ArticleMeta,
+  ArticleTitle,
+  ButtonGroup,
+} from '../styles/articles'
 
 export const QnaDetail = () => {
   const navigate = useNavigate()
@@ -135,7 +90,10 @@ export const QnaDetail = () => {
         </ArticleHeader>
         <ArticleMeta>
           <span>작성자 : {board.nickname}</span>
-          <span style={{ marginLeft: '30px' }}>{board.date}</span>
+          <span style={{ marginLeft: '35px' }}>조회수 : {board.views}</span>
+          <span style={{ marginLeft: '35px' }}>
+            등록일 : {formatDateDetail(board.date)}
+          </span>
         </ArticleMeta>
         <ArticleContent>{board.content}</ArticleContent>
 
