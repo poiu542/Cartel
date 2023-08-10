a60a689bd317bd52969dd494605e87f5bac51a00
bluest12 <bluest12@naver.com>
2023-08-10 15:46:57 +0900

    ës3 ì외 기능 제거

--- BACKEND/src/main/java/com/ssafy/cartel/repository/client/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.cartel.repository.client;
-
-import com.ssafy.cartel.domain.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
