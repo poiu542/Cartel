e31744232548818826bbc92d058d64440a16f1ea
이경원 <jbs6606@naver.com>
2023-08-14 14:01:34 +0900

    counsel관련 커밋

--- BACKEND/src/main/java/com/ssafy/cartel/repository/ConsultingRepository.java
+++ BACKEND/src/main/java/com/ssafy/cartel/repository/ConsultingRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ConsultingRepository extends JpaRepository<Consulting, Integer> {
 
-    @Query("select c from Consulting c join fetch c.clientId ")
-    Consulting selectConsulting(Integer id);
 }
