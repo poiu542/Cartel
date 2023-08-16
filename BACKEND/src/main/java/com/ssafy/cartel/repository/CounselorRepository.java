aac4a5168aa3dd9b1b9f7498020a5f3e3145fc6f
안도희 <adh1120@naver.com>
2023-08-16 15:44:07 +0900

    file: curriculum

--- BACKEND/src/main/java/com/ssafy/cartel/repository/CounselorRepository.java
+++ BACKEND/src/main/java/com/ssafy/cartel/repository/CounselorRepository.java
@@ -1,8 +1,15 @@
 package com.ssafy.cartel.repository;
 
 
+import com.ssafy.cartel.domain.Career;
+import com.ssafy.cartel.domain.Counsel;
 import com.ssafy.cartel.domain.Counselor;
+import com.ssafy.cartel.dto.CounselorDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CounselorRepository extends JpaRepository<Counselor,Integer> {
+
+
 }
