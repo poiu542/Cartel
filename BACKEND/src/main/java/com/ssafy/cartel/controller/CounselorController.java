6e99e9c19d8eb1e1422a8f04afbc530b5702f9cd
bluest12 <bluest12@naver.com>
2023-08-17 17:04:50 +0900

    requestpart 변경

--- BACKEND/src/main/java/com/ssafy/cartel/controller/CounselorController.java
+++ BACKEND/src/main/java/com/ssafy/cartel/controller/CounselorController.java
@@ -35,7 +35,7 @@ public class CounselorController {
     private final CounselorRepository counselorRepository;
 
     @PostMapping(value = "/signup/counselor", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> signupCounselor(@RequestPart CounselorSignupRequest request, @RequestPart(value = "file1") MultipartFile multipartFile1, @RequestPart(value = "file2") MultipartFile multipartFile2, @RequestPart(value = "file3") MultipartFile multipartFile3) throws IOException, IOException {
+    public ResponseEntity<String> signupCounselor(@RequestPart("request") CounselorSignupRequest request, @RequestPart("file1") MultipartFile multipartFile1, @RequestPart("file2") MultipartFile multipartFile2, @RequestPart("file3") MultipartFile multipartFile3) throws IOException, IOException {
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("______________"+multipartFile1);
         System.out.println(request.getCounselorDto().getCompany());
